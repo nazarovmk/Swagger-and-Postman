625a499470a86b231c92e81cdf25207d12929095
Muhammadnazar <muhammadnazarnazarov25@gmail.com>
2025-03-23 19:18:57 +0500

    Swagger-and-Postman

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACQzAynxbyNtc_rdHPBHgGkMn1hp27NT0",
-  authDomain: "apps-c3d31.firebaseapp.com",
-  projectId: "apps-c3d31",
-  storageBucket: "apps-c3d31.firebasestorage.app",
-  messagingSenderId: "604414308300",
-  appId: "1:604414308300:web:a9dc7caf5a0fd2bdaf3769",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
